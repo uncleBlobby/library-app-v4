3e252995ea52f889193b791431fce93b2d8ca148
uncleBlobby <dc.christianson@gmail.com>
2021-05-12 20:04:25 -0600

    added book card display

--- main.js
+++ main.js
@@ -10,17 +10,41 @@ let bookAddCheck = document.getElementsByClassName("addBook")[0];
 
 let addBookForm = document.getElementById("addBookForm");
 
+let libraryGrid = document.getElementById("libraryDisplay");
+
 //let btitle = addBookForm.elements[0];
+
+function addCardToLibrary(newBook){
+
+        let newLibraryCard = document.createElement("div");
+        newLibraryCard.classList.add("libraryCard");
+        libraryGrid.appendChild(newLibraryCard);
+        newLibraryCard.innerText = newBook.info;
+
+}
+
 
 function submitForm(){
     myLibrary[myLibrary.length] = new Book(btitle.value, bauthor.value, bpages.value, bisread.checked);
     console.log("form submitted");
     console.log("library length: " + myLibrary.length);
     console.log(myLibrary);
+    addCardToLibrary(myLibrary[myLibrary.length - 1]);
 }
+
+
+
 
 addBookButton.onclick = function() {
     modal.style.display = "block";
+    resetFormInputs();
+}
+
+function resetFormInputs(){
+    addBookForm.elements[0].value = "Title";
+    addBookForm.elements[1].value = "Author";
+    addBookForm.elements[2].value = "Pages";
+    addBookForm.elements[3] = false;
 }
 
 modalCloser.onclick = function() {
@@ -29,6 +53,7 @@ modalCloser.onclick = function() {
 
 bookAddCheck.onclick = function() {
     submitForm(btitle);
+    modal.style.display = "none";
     /*
     myLibrary[myLibrary.length] = new Book(btitle, bauthor, bpages, bisread);
     modal.style.display = "none";
@@ -49,7 +74,7 @@ function Book(title, author, pages, isRead){
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    this.info = title + ' by ' + author + ', ' + pages + ' pages ' + 'read: ' + isRead;
+    this.info = 'Title: ' + title + '\r\nAuthor: ' + author + '\r\nPages: ' + pages + ' pages ' + '\r\nRead: ' + isRead;
 
 }
 
